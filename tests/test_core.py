fdfd081cace389ff4e61e2ca1fecd4e94b29370a
umLu <umluiz@gmail.com>
2021-08-05 13:13:04 -0300

    Caption test

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,3 +8,9 @@ class YoutubeSearch(unittest.TestCase):
         df_shape = search("Test", maxres=25).df.shape
         self.assertEqual(df_shape[0], 25)
         self.assertEqual(df_shape[1], 12)
+
+    def test_caption(self):
+        df_caption = search("Test", caption=True).df['video_caption']
+        self.assertTrue(
+            len([_ for _ in df_caption.to_list() if _ is None]) > 0
+            )
