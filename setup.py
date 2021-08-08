dce66dc7b749ad7d79132ade84e002081b0ff3d7
umLu <umluiz@gmail.com>
2021-08-08 19:14:23 -0300

    Raise maxres limit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="youtubeinfo",
-    version="0.1.3",
+    version="0.1.4",
     author="Luiz P.",
     author_email="umluiz@gmail.com",
     description="Get YouTube data for analysis",
--- youtubeinfo/core.py
+++ youtubeinfo/core.py
@@ -1,6 +1,7 @@
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 import requests
+import time
 import pandas as pd
 import os
 from youtube_transcript_api import YouTubeTranscriptApi
@@ -12,29 +13,50 @@ from youtubeinfo.config.constants import YOUTUBE_API_URL
 class search:
     """Main class for YouTube search."""
 
+    DEFAULT_MAX_RES = 50
+
     def __init__(self,
                  term: str,
                  caption: bool = False,
                  maxres: int = 50,
                  accepted_caption_lang: list = ['en'],
                  developer_key: str = None) -> None:
+        self._accepted_caption_lang = accepted_caption_lang
         if developer_key is None:
             self._developer_key = os.environ['YOUTUBE_DEVELOPER_KEY']
         else:
             self._developer_key = developer_key
-        self._accepted_caption_lang = accepted_caption_lang
-        self.raw = self._search_from_term(term, maxres)
-        if len(self.raw['items']):
-            self.df = self._build_dataframe(caption=caption)
+        self.raw = self._consolidate_search(term, maxres)
+        self.df = self._build_dataframe(caption=caption)
+
+    def _consolidate_search(self,
+                            term: str,
+                            maxres: int) -> list:
+        if maxres <= self.DEFAULT_MAX_RES:
+            results = maxres
+            maxres = 0
         else:
-            print('No results. The DataFrame attribute will be None')
-            self.df = None
+            results = self.DEFAULT_MAX_RES
+            maxres -= self.DEFAULT_MAX_RES
+
+        search_list = self._search_from_term(term, results)
+        consolidated_search = [search_list]
+        while 'nextPageToken' in search_list.keys() and maxres > 0:
+            time.sleep(0.1)  # Avoid request overload
+            consolidated_search.append(
+                self._search_from_term(term,
+                                       results,
+                                       pageToken=search_list['nextPageToken'])
+                )
+            maxres -= self.DEFAULT_MAX_RES
+        return consolidated_search
 
     def _search_from_term(self,
                           term: str,
-                          maxres: int = 50):
+                          maxres: int = 50,
+                          pageToken: str = None) -> dict:
         try:
-            search_list = self._search_request(term, maxres)
+            search_list = self._search_request(term, maxres, pageToken)
             # Validate response
             if isinstance(search_list, dict):
                 if not set(search_list.keys()).issuperset(
@@ -54,58 +76,62 @@ class search:
 
     def _search_request(self,
                         term: str,
-                        maxres: int = 50):
+                        maxres: int = 50,
+                        pageToken: str = None) -> dict:
         """Query YouTube."""
         youtube = build(YOUTUBE_API_SERVICE_NAME,
                         YOUTUBE_API_VERSION,
                         developerKey=self._developer_key)
-
-        # Call the search.list method to retrieve results matching the
-        # specified query term.
         search_response = youtube.search().list(
             q=term,
             part="id,snippet",
-            maxResults=maxres
+            maxResults=maxres,
+            pageToken=pageToken
         ).execute()
         return search_response
 
     def _build_dataframe(self,
                          caption: bool = False):
         appended_data = []
-        for search_item in self.raw['items']:
-            video_id = search_item['id']['videoId']
-            search_results = search_item['snippet']
-            search_results.pop("thumbnails", None)
-            search_results['videoId'] = video_id
-            video_statistics = self.get_statistics(video_id)
-            video_statistics['videoId'] = video_id
-            if caption:
-                _, video_caption = self.get_captions(video_id)
-                video_caption = {'video_caption': video_caption}
-                video_metadata = {**search_results,
-                                  **video_statistics,
-                                  **video_caption}
-            else:
-                video_metadata = {**search_results,
-                                  **video_statistics}
+        for search_req in self.raw:
+            for search_item in search_req['items']:
+                video_id = search_item['id']['videoId']
+                search_results = search_item['snippet']
+                search_results.pop("thumbnails", None)
+                search_results['videoId'] = video_id
+                video_statistics = self.get_statistics(video_id)
+                video_statistics['videoId'] = video_id
+                if caption:
+                    _, video_caption = self.get_captions(video_id)
+                    video_caption = {'video_caption': video_caption}
+                    video_metadata = {**search_results,
+                                      **video_statistics,
+                                      **video_caption}
+                else:
+                    video_metadata = {**search_results,
+                                      **video_statistics}
 
-            video_metadata = pd.DataFrame.from_dict([video_metadata])
-            appended_data.append(video_metadata)
-        appended_data = pd.concat(appended_data)
-        appended_data.dropna(axis=0,
-                             how='any',
-                             inplace=True,
-                             subset=['likeCount',
-                                     'dislikeCount',
-                                     'viewCount'])
-        df_youtube = appended_data.astype({"likeCount": int,
-                                           "dislikeCount": int,
-                                           "viewCount": int})
-        df_youtube['publishedAt'] = pd.to_datetime(
-            df_youtube['publishedAt'])
-        df_youtube.set_index('videoId',
-                             inplace=True)
-        return df_youtube
+                video_metadata = pd.DataFrame.from_dict([video_metadata])
+                appended_data.append(video_metadata)
+        if len(appended_data):
+            appended_data = pd.concat(appended_data)
+            appended_data.dropna(axis=0,
+                                 how='any',
+                                 inplace=True,
+                                 subset=['likeCount',
+                                         'dislikeCount',
+                                         'viewCount'])
+            df_youtube = appended_data.astype({"likeCount": int,
+                                               "dislikeCount": int,
+                                               "viewCount": int})
+            df_youtube['publishedAt'] = pd.to_datetime(
+                df_youtube['publishedAt'])
+            df_youtube.set_index('videoId',
+                                 inplace=True)
+            return df_youtube
+        else:
+            print('No results. The DataFrame attribute will be None')
+            return None
 
     def get_statistics(self,
                        video_id: str) -> dict:
